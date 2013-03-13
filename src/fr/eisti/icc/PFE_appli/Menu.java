c3e01fbfdf1f081d9acf2818cf1ccbba17d7c4e5
Eric Hostalery <hostalerye@gmail.com>
2013-03-13 10:49:01 +0100

    skeleton

---
+++ src/fr/eisti/icc/PFE_appli/Menu.java
@@ -0,0 +1,23 @@
+package fr.eisti.icc.PFE_appli;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hostalerye
+ * Date: 13/03/13
+ * Time: 10:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class Menu extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Hello World, Android");
+        setContentView(text);
+    }
+}
