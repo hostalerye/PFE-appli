c9e1f6d3ecfcf3c6433c0ab312d8d73aacf8e320
Vivien Alger <vivien.alger@gmail.com>
2013-03-26 10:53:30 +0100

    Asking and storing phone number

---
+++ src/fr/eisti/icc/PFE_appli/GCMIntentService.java
@@ -0,0 +1,29 @@
+package fr.eisti.icc.PFE_appli;
+
+import android.content.Context;
+import android.content.Intent;
+import com.google.android.gcm.GCMBaseIntentService;
+
+public class GCMIntentService extends GCMBaseIntentService {
+
+    @Override
+    protected void onMessage(Context context, Intent intent) {
+
+    }
+
+    @Override
+    protected void onError(Context context, String errorId) {
+
+    }
+
+    @Override
+    protected void onRegistered(Context context, String regId) {
+
+    }
+
+    @Override
+    protected void onUnregistered(Context context, String regId) {
+
+    }
+
+}
--- src/fr/eisti/icc/PFE_appli/Menu.java
+++ src/fr/eisti/icc/PFE_appli/Menu.java
@@ -2,20 +2,41 @@ package fr.eisti.icc.PFE_appli;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * Created with IntelliJ IDEA.
- * User: hostalerye
- * Date: 13/03/13
- * Time: 10:34
- * To change this template use File | Settings | File Templates.
- */
+import java.io.*;
+
 public class Menu extends Activity{
+
+    private String getPhoneNumber(){
+        String result = "";
+        // Read phoneNumber from internal storage
+        try {
+            FileInputStream fis = openFileInput(getString(R.string.filename));
+            InputStreamReader reader = new
+                    InputStreamReader(fis);
+            BufferedReader buff = new BufferedReader(
+                    reader);
+            result = buff.readLine();
+            buff.close();
+            reader.close();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.e("FileNotFound","Can't find config file");
+        } catch (IOException e){
+            Log.e("IO","Can't open config file");
+        }
+
+        return result;
+    }
 
     private String launchBenchmark(CharSequence bench_name){
         if(bench_name.equals("Pi")) {
@@ -29,13 +50,9 @@ public class Menu extends Activity{
         return "";
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-
-        final Button button = (Button)findViewById(R.id.launchButton);
-        button.setOnClickListener(new View.OnClickListener() {
+    private void launchButtonSetUp(){
+        final Button launchButton = (Button)findViewById(R.id.launchButton);
+        launchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder
@@ -59,5 +76,66 @@ public class Menu extends Activity{
                 dialog.show();
             }
         });
+    }
+
+    private void phoneButtonSetUp(){
+        final Button phoneButton = (Button) findViewById(R.id.phoneButton);
+        phoneButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(Menu.this);
+
+                // Set dialog layout
+                builder.setView(getLayoutInflater().inflate(
+                        R.layout.dialog_phone_number,
+                        null, false));
+
+                // Set dialog buttons
+                builder.setPositiveButton(R.string.register,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Needed for context
+                                Dialog d = (Dialog) dialog;
+                                EditText edit = (EditText)d.findViewById(R
+                                        .id
+                                        .phoneNumber);
+                                String phoneNumber = edit.getText().toString()
+                                        .trim();
+
+                                // Write to internal storage
+                                String filename = getString(R.string.filename);
+                                try{
+                                    FileOutputStream fos = openFileOutput(filename,
+                                            Context.MODE_PRIVATE);
+                                    fos.write(phoneNumber.getBytes());
+                                    fos.close();
+                                } catch (FileNotFoundException e) {
+                                    Log.d("DEBUG","Can't create File!");
+                                } catch (IOException e) {
+                                    Log.d("DEBUG","Can't make file output " +
+                                            "stream!");
+                                }
+                            }
+                        });
+
+                builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        launchButtonSetUp();
+        phoneButtonSetUp();
     }
 }
