a747169abdd12afbb74837c50c867794e607416b
Vivien Alger <vivien.alger@gmail.com>
2013-04-02 16:53:47 +0200

    Refactoring into utils

--- src/fr/eisti/icc/PFE_appli/GCMIntentService.java
+++ src/fr/eisti/icc/PFE_appli/GCMIntentService.java
@@ -5,102 +5,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import com.google.android.gcm.GCMBaseIntentService;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.util.InetAddressUtils;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
-import java.io.*;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GCMIntentService extends GCMBaseIntentService {
+
+    Utils utils = new Utils();
 
     private void returnPing(Bundle extras){
         String ping_id = extras.getString("ping_id");
 
         Map<String,String> tmp = new HashMap<String, String>();
         tmp.put("ping_id",ping_id);
-        tmp.put("phone_number",getPhoneNumber());
-        tmp.put("ip",getIP());
+        tmp.put("phone_number",utils.getPhoneNumber());
+        tmp.put("ip",utils.getIP());
 
         JSONObject payload = new JSONObject(tmp);
 
-        postRequest("/messages/ping",payload);
-    }
-
-    private String getIP(){
-        String sAddr = "";
-        try {
-            List<NetworkInterface> interfaces = Collections.list(NetworkInterface
-                    .getNetworkInterfaces());
-            for(NetworkInterface intf : interfaces){
-                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
-                for (InetAddress addr : addrs) {
-                    if (!addr.isLoopbackAddress()) {
-                        sAddr = addr.getHostAddress().toUpperCase();
-                    }
-                }
-            }
-        } catch (SocketException e) {
-            Log.e("INET SOCKET","Can't connect to socket to determine IP " +
-                    "adress");
-        }
-
-        return sAddr;
-    }
-
-    private String getPhoneNumber(){
-        String result = "";
-        // Read phoneNumber from internal storage
-        try {
-            FileInputStream fis = openFileInput(getString(R.string.filename));
-            InputStreamReader reader = new
-                    InputStreamReader(fis);
-            BufferedReader buff = new BufferedReader(
-                    reader);
-            result = buff.readLine();
-            buff.close();
-            reader.close();
-            fis.close();
-        } catch (FileNotFoundException e) {
-            Log.e("FileNotFound","Can't find config file");
-        } catch (IOException e){
-            Log.e("IO","Can't open config file");
-        }
-
-        return result;
-    }
-
-    private void postRequest(String url, JSONObject json){
-        HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(getString(R.string.nodeServer) + url);
-
-        try {
-            // Add the JSON to the POST request
-            StringEntity se = new StringEntity(json.toString());
-            post.setEntity(se);
-            post.setHeader("Content-type","application/json");
-            post.setHeader("Accept","application/json");
-        } catch (UnsupportedEncodingException e) {
-            Log.e("Encoding error","Can't change JSON object to string " +
-                    "entity");
-        }
-
-        try {
-            client.execute(post);
-        } catch (IOException e) {
-            Log.d("IO on POST","Can't execute post request to node server");
-        }
+        utils.postRequest("/messages/ping",payload);
     }
 
     @Override
@@ -124,12 +48,12 @@ public class GCMIntentService extends GCMBaseIntentService {
         // Fill map with infos
         Map<String, String> tmp = new HashMap<String,String>();
         tmp.put("reg_id",regId);
-        tmp.put("phone_number",getPhoneNumber());
+        tmp.put("phone_number",utils.getPhoneNumber());
 
         // Transform map into JSONObject
         JSONObject upload = new JSONObject(tmp);
 
-        postRequest("/devices/register",upload);
+        utils.postRequest("/devices/register",upload);
 
     }
 
@@ -142,7 +66,7 @@ public class GCMIntentService extends GCMBaseIntentService {
         // Transform map into JSONObject
         JSONObject upload = new JSONObject(tmp);
 
-        postRequest("/devices/unregister",upload);
+        utils.postRequest("/devices/unregister",upload);
     }
 
 }
---
+++ src/fr/eisti/icc/PFE_appli/Utils.java
@@ -0,0 +1,108 @@
+package fr.eisti.icc.PFE_appli;
+
+import android.app.Activity;
+import android.util.Log;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpResponse;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Collections;
+import java.util.List;
+
+public class Utils extends Activity {
+    public String getIP(){
+        String sAddr = "";
+        try {
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface
+                    .getNetworkInterfaces());
+            for(NetworkInterface intf : interfaces){
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        sAddr = addr.getHostAddress().toUpperCase();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e("INET SOCKET","Can't connect to socket to determine IP " +
+                    "adress");
+        }
+
+        return sAddr;
+    }
+
+    public String getPhoneNumber(){
+        String result = "";
+        // Read phoneNumber from internal storage
+        try {
+            FileInputStream fis = openFileInput(getString(R.string.filename));
+            InputStreamReader reader = new
+                    InputStreamReader(fis);
+            BufferedReader buff = new BufferedReader(
+                    reader);
+            result = buff.readLine();
+            buff.close();
+            reader.close();
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.e("FileNotFound","Can't find config file");
+        } catch (IOException e){
+            Log.e("IO","Can't open config file");
+        }
+
+        return result;
+    }
+
+    public void postRequest(String url, JSONObject json){
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(getString(R.string.nodeServer) + url);
+
+        try {
+            // Add the JSON to the POST request
+            StringEntity se = new StringEntity(json.toString());
+            post.setEntity(se);
+            post.setHeader("Content-type","application/json");
+            post.setHeader("Accept","application/json");
+        } catch (UnsupportedEncodingException e) {
+            Log.e("Encoding error", "Can't change JSON object to string " +
+                    "entity");
+        }
+
+        try {
+            client.execute(post);
+        } catch (IOException e) {
+            Log.d("IO on POST","Can't execute post request to node server");
+        }
+    }
+
+    public HttpResponse getRequest(String url, JSONObject json){
+        HttpClient client = new DefaultHttpClient();
+        HttpGet get = new HttpGet(getString(R.string.nodeServer) + url);
+        HttpParams params = new BasicHttpParams();
+
+        // Add the JSON to the POST request
+        params.setParameter("json",json);
+        //get.setHeader("Content-type", "application/json");
+        //get.setHeader("Accept", "application/json");
+
+        HttpResponse response = null;
+        try {
+            response = client.execute(get);
+        } catch (IOException e) {
+            Log.d("IO on POST","Can't execute post request to node server");
+        }
+
+        return response;
+    }
+}
